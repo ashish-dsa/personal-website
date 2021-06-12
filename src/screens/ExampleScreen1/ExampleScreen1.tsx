e087dd1a2a2d7cffc1ab33db34fd57b99c3b866c
Ashish D'sa <ashd621@gmail.com>
2021-06-13 01:27:26 +0530

    Updated src/screens/ExampleScreen1/ExampleScreen1.tsx

--- src/screens/ExampleScreen1/ExampleScreen1.tsx
+++ src/screens/ExampleScreen1/ExampleScreen1.tsx
@@ -1,40 +1,44 @@
 import {NavigationProp} from "@react-navigation/core";
-import Button from "components/Button";
-import Card, {CardActions, CardCover, CardTitle} from "components/Card";
+import AnimatedText from "components/AnimatedText";
 import {ROUTES} from "navigation/constants";
-import {default as React, ReactElement, useEffect} from "react";
-import {ScrollView, StyleSheet} from "react-native";
-import {Appbar} from "react-native-paper";
+import {default as React, ReactElement} from "react";
+import {ScrollView, StyleSheet, View} from "react-native";
+import {Button, Card} from "react-native-paper";
+import Swiper from "react-native-web-swiper";
 
 interface IExampleScreen1Props {
   navigation: NavigationProp<any>;
   theme: ReactNativePaper.Theme;
 }
 const ExampleScreen1: React.FC<IExampleScreen1Props> = (props): ReactElement => {
-  useEffect(() => {
-    console.log("ExampleScreen1 mounted");
-    return () => {
-      console.log("ExampleScreen1 unmounted");
-    };
-  }, [props]);
   return (
     <ScrollView style={styles.backgroundStyle}>
-      <Appbar.Header>
-        <Appbar.Content title="Example Screen 1" subtitle="" />
-      </Appbar.Header>
-      {[1, 1, 1].map((value, index) => {
-        return (
-          <Card style={styles.card} key={index}>
-            <CardTitle title="Card Title" subtitle="Card Subtitle" />
-            <CardCover source={{uri: "https://picsum.photos/70" + index.toString()}} />
-            <CardActions>
-              <Button mode="contained" onPress={() => props.navigation.navigate(ROUTES.ExampleScreen2)}>
-                Go to next screen
-              </Button>
-            </CardActions>
-          </Card>
-        );
-      })}
+      <View style={styles.swiper}>
+        <Swiper
+          loop
+          timeout={-2.5}
+          controlsProps={{
+            dotActiveStyle: {backgroundColor: "red"},
+            dotsTouchable: true,
+            prevPos: false,
+            nextPos: false,
+          }}>
+          {[1, 1, 1].map((value, index) => {
+            return (
+              <Card style={styles.card} key={index}>
+                <Card.Title title="Card Title" subtitle="Card Subtitle" />
+                <Card.Cover source={{uri: "https://picsum.photos/70" + index.toString()}} />
+                <Card.Actions>
+                  <Button mode="contained" onPress={() => props.navigation.navigate(ROUTES.ExampleScreen2)}>
+                    Go to next screen
+                  </Button>
+                </Card.Actions>
+              </Card>
+            );
+          })}
+        </Swiper>
+      </View>
+      <AnimatedText />
     </ScrollView>
   );
 };
@@ -46,6 +50,9 @@ const styles = StyleSheet.create({
   card: {
     marginHorizontal: "2%",
     marginVertical: "2%",
+  },
+  swiper: {
+    height: "90%",
   },
 });
 
